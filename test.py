d2608880799bcd69073a96d34d1dac934d71bf08
Oversize204 <a.rittinger+git@gmail.com>
2021-06-18 12:08:37 +0000

    works on gitpod

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 import os
+import cvb
 
-print("Hello")
+print(os.environ["CVB"])
+
+device = cvb.DeviceFactory.open(os.path.join(os.environ["CVB"], "drivers", "CVMock.vin"))
+stream = device.stream
+
+stream.start()
+
+for i in range(5):
+    image, status = stream.wait()
+    if status == cvb.WaitStatus.Ok:
+        image.save(os.path.join(".", ".cvb", "test" + str(i) + ".jpg"))
+
+stream.abort()
